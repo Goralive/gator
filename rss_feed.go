291669532b2f241027e2278f92c418a3a9c8ea84
Igor Nosovskyi <nosovskyigor@gmail.com>
2025-01-16 22:57:59 +0100

    add rss struct

---
+++ rss_feed.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"context"
+	"net/http"
+	"time"
+)
+
+type RSSFeed struct {
+	Channel struct {
+		Title       string    `xml:"title"`
+		Link        string    `xml:"link"`
+		Description string    `xml:"description"`
+		Item        []RSSItem `xml:"item"`
+	} `xml:"channel"`
+}
+
+type RSSItem struct {
+	Title       string `xml:"title"`
+	Link        string `xml:"link"`
+	Description string `xml:"description"`
+	PubDate     string `xml:"pubDate"`
+}
+
+func fetchFeed(ctx context.Context, feedUrl string) (*RSSFeed, error) {
+	httpClient := http.Client{
+		Timeout: 10 * time.Second,
+	}
+
+	var rssFeed RSSFeed
+	return &rssFeed, nil
+}
