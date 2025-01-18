3baa1e66301ffb609a72f0c9b48f33c51d8e322b
Igor Nosovskyi <nosovskyigor@gmail.com>
2025-01-18 22:09:49 +0100

    add aggregator func

--- rss_feed.go
+++ rss_feed.go
@@ -2,6 +2,9 @@ package main
 
 import (
 	"context"
+	"encoding/xml"
+	"html"
+	"io"
 	"net/http"
 	"time"
 )
@@ -22,12 +25,40 @@ type RSSItem struct {
 	PubDate     string `xml:"pubDate"`
 }
 
-func fetchFeed(ctx context.Context, feedUrl string) (*RSSFeed, error) {
+func fetchFeed(ctx context.Context, feedURL string) (*RSSFeed, error) {
 	httpClient := http.Client{
 		Timeout: 10 * time.Second,
 	}
-	req, err := http.NewRequestWithContext(ctx, "GET", feedUrl, nil)
+	req, err := http.NewRequestWithContext(ctx, "GET", feedURL, nil)
+	if err != nil {
+		return nil, err
+	}
+
+	req.Header.Set("User-Agent", "gator")
+	resp, err := httpClient.Do(req)
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+
+	dat, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return nil, err
+	}
 
 	var rssFeed RSSFeed
+	err = xml.Unmarshal(dat, &rssFeed)
+	if err != nil {
+		return nil, err
+	}
+
+	rssFeed.Channel.Title = html.UnescapeString(rssFeed.Channel.Title)
+	rssFeed.Channel.Description = html.UnescapeString(rssFeed.Channel.Description)
+	for i, item := range rssFeed.Channel.Item {
+		item.Title = html.UnescapeString(item.Title)
+		item.Description = html.UnescapeString(item.Description)
+		rssFeed.Channel.Item[i] = item
+	}
+
 	return &rssFeed, nil
 }
