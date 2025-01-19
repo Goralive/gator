a2c9f770d6a2a4c7ab8f54209fabe447057071ef
Igor Nosovskyi <nosovskyigor@gmail.com>
2025-01-19 22:47:04 +0100

    adding db

---
+++ sql/schema/002_feeds.sql
@@ -0,0 +1,12 @@
+-- +goose Up
+
+
+CREATE TABLE feeds (
+  id UUID NOT NULL PRIMARY KEY(id),
+  created_at TIMESTAMP NOT NULL,
+  updated_at TIMESTAMP NOT NULL,
+  name VARCHAR(255) NOT NULL,
+  url TEXT NOT NULL UNIQUE,
+  user_id UUID NOT NULL FOREIGN KEY(user_id) REFERENCES users(id) ON DELETE CASCADE,
+);
+
