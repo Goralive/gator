54bcf7cf41fbe322019c8d4ebc3096983aa37816
Igor Nosovskyi <nosovskyigor@gmail.com>
2025-02-02 21:30:36 +0100

    meh

--- sql/schema/003_feed_follows.sql
+++ sql/schema/003_feed_follows.sql
@@ -6,6 +6,7 @@ CREATE TABLE feed_foolows (
   created_at TIMESTAMP NOT NULL,
   updated_at TIMESTAMP NOT NULL,
   feed_id UUID NOT NULL REFERENCES feeds(id) ON DELETE CASCADE,
+
   user_id UUID NOT NULL REFERENCES users(id) ON DELETE CASCADE,
 );
 
